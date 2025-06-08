21d5f0e8e55042833f4d511697ff173b5a57d801
Javier Alarcon <ja.ge.al.al@pragma.com.co>
2025-06-07 19:13:57 -0500

    [core] add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   vite: {
     resolve: {
       alias: {
